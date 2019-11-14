fc707e38c72f99010deeecceb39a099d685164a2
Tom Dugan <tdugan@paloaltonetworks.com>
2019-11-13 22:16:28 -0500

    ...

--- outputs.tf
+++ outputs.tf
@@ -6,10 +6,10 @@ output "dataplane_cpu_scale_down_policy" {
   value = aws_autoscaling_policy.dataplane_cpu-scale_down.arn
 }
 output "active_session_scale_up_policy" {
-  value = aws_autoscaling_policy.active_session-scale_up.arn
+  value = aws_autoscaling_policy.active_sessions-scale_up.arn
 }
 output "active_session_scale_down_policy" {
-  value = aws_autoscaling_policy.active_session-scale_down.arn
+  value = aws_autoscaling_policy.active_sessions-scale_down.arn
 }
 
 output "asg" {
