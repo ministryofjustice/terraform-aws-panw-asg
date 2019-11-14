25dae626cb9c65a105ee3d12ecdd6d95ae1cc13a
Tom Dugan <tdugan@paloaltonetworks.com>
2019-11-13 22:14:19 -0500

    should test prior to push...

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
 output "dataplane_cpu_scale_up_policy" {
-  value = aws_autoscaling_policy.dataplane_cpu-scale_out.arn
+  value = aws_autoscaling_policy.dataplane_cpu-scale_up.arn
 }
 
 output "dataplane_cpu_scale_down_policy" {
   value = aws_autoscaling_policy.dataplane_cpu-scale_down.arn
 }
 output "active_session_scale_up_policy" {
-  value = aws_autoscaling_policy.dataplane_cpu-scale_up.arn
+  value = aws_autoscaling_policy.active_session-scale_up.arn
 }
 output "active_session_scale_down_policy" {
   value = aws_autoscaling_policy.active_session-scale_down.arn
